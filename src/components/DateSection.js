f932c8f3b544ce643c395d78f83350bcb94aa65a
Robert <robert@bigwings.de>
2017-05-21 12:35:29 +0200

    current date is scrolled into view on load

--- src/components/DateSection.js
+++ src/components/DateSection.js
@@ -10,52 +10,58 @@ class DateSection extends Component {
         this.dropzone = 0;
     }
 
-  makeMoment(date) {
-    return moment(date, moment.ISO_8601).calendar(null, {
-        sameDay: '[Today]',
-        nextDay: '[Tomorrow]',
-        nextWeek: 'MMMM YYYY, [Next] dddd',
-        lastDay: '[Yesterday]',
-        lastWeek: 'MMMM YYYY, [Last] dddd',
-        sameElse: 'MMMM YYYY, dddd'
-    })
-  }
-
-  onDrop(e) {
-    if(e.dataTransfer.getData("due") !== this.props.date) {
-        this.props.taskChange(
-            e.dataTransfer.getData("id"), 
-            "due", 
-            moment(this.props.date, moment.ISO_8601)
-        )
+    componentDidMount() {
+        if(moment(this.props.date, moment.ISO_8601).isSame(moment(),"d")) {
+            this.el.scrollIntoViewIfNeeded()
+        }
     }
-  }
 
-  render() {
-    return (
-        <DropableDiv onDrop={this.onDrop.bind(this)} >
-            <div className="row datearea">
-                <div className="day">
-                    {this.props.date == null ? "0" : moment(this.props.date, moment.ISO_8601).format("D")}
+    makeMoment(date) {
+        return moment(date, moment.ISO_8601).calendar(null, {
+            sameDay: '[Today]',
+            nextDay: '[Tomorrow]',
+            nextWeek: 'MMMM YYYY, [Next] dddd',
+            lastDay: '[Yesterday]',
+            lastWeek: 'MMMM YYYY, [Last] dddd',
+            sameElse: 'MMMM YYYY, dddd'
+        })
+    }
+
+    onDrop(e) {
+        if (e.dataTransfer.getData("due") !== this.props.date) {
+            this.props.taskChange(
+                e.dataTransfer.getData("id"),
+                "due",
+                moment(this.props.date, moment.ISO_8601)
+            )
+        }
+    }
+
+    render() {
+        return (
+            <DropableDiv onDrop={this.onDrop.bind(this)} >
+                <div className="row datearea" ref={ref => this.el = ref}>
+                    <div className="day">
+                        {this.props.date == null ? "0" : moment(this.props.date, moment.ISO_8601).format("D")}
+                    </div>
+                    <div className={
+                        moment(this.props.date, moment.ISO_8601).isSame(moment(), "d") ? "date today" : "date"
+                    }>
+                        {this.props.date == null ? "No Due Date" : this.makeMoment(this.props.date)}
+                    </div>
                 </div>
-                <div className={
-                    moment(this.props.date, moment.ISO_8601).isSame(moment(),"d") ? "date today" : "date"
-                }>
-                    {this.props.date == null ? "No Due Date" : this.makeMoment(this.props.date)}
-                </div>
-            </div>
-            <ul>
-                {this.props.tasks.map(task =>
-                    <Task
-                    taskChange={this.props.taskChange}
-                    key={task.id}
-                    {...task}
-                    />
-                )}
-            </ul>
-        </DropableDiv>
-    );
-  }
+                <ul>
+                    {this.props.tasks.map(task =>
+                        <Task
+                            taskChange={this.props.taskChange}
+                            key={task.id}
+                            {...task}
+                        />
+                    )}
+                </ul>
+            </DropableDiv>
+        );
+    }
 
 }
 export default DateSection;
