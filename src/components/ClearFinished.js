58db81d82ec871b5f7459b59c5952e7133a65666
Robert <robert@bigwings.de>
2017-11-21 22:16:11 +0100

    removed unneccessary import

--- src/components/ClearFinished.js
+++ src/components/ClearFinished.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux'
 import { clearFinishedTasks } from '../actions/tasks'
 import React from 'react'
-import PropTypes from 'prop-types'
 
 export const ClearButton = (props) => {
     return(
