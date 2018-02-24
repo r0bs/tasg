62445f9834274518490b99038567813072b899f6
Robert <robert@bigwings.de>
2018-02-24 22:39:52 +0100

    added deployment script

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,6 @@ export default class App extends Component {
     return (
       <div className="container-fluid">
         <div className="row">
-          <h1>test</h1>
           <Sidebar>
             <Logo />  
             <LoginButtonContainer />
